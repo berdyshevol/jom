5cda2fe585319d6307c46e391e086b32f2c6cbc0
berdyshevol <berdyshevo@gmail.com>
2020-06-17 11:39:43 +0300

    sprint2 tast 5

---
+++ Sprints/sprint02/src/task05/MyUtils.java
@@ -0,0 +1,55 @@
+package task05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface IPerimeter {
+    public double getPerimeter();
+}
+
+class Rectang implements IPerimeter {
+    private double width;
+    private double height;
+
+    public Rectang(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (this.width + this.height);
+    }
+}
+class Square implements IPerimeter {
+    private double width;
+
+    public Square(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * this.width;
+    }
+}
+public class MyUtils {
+    public double sumPerimeter(List<?> figures) {
+        if (figures.stream().anyMatch(figure -> figure == null)) {
+            return 0;
+        }
+        return figures
+                .stream()
+                .map(figure -> ((IPerimeter)figure).getPerimeter())
+                .reduce(0.0, Double::sum);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<IPerimeter> list = new ArrayList<>();
+        list.add(new Square(4.0));
+        list.add(new Square(5.0));
+        list.add(new Rectang(2.0, 3.0));
+        list.add(new Rectang(2.0, 3.0));
+        System.out.println(new MyUtils().sumPerimeter(list));
+    }
+}
---
+++ Sprints/sprint02/src/task06/MyUtils.java
@@ -0,0 +1,23 @@
+package task06;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+abstract class Shape {
+    abstract double getArea();
+}
+class Circle extends Shape {
+    // Code
+}
+class Rectangle extends Shape {
+    // Code
+}
+public class MyUtils {
+    public List<Shape> maxAreas(List<Shape> shapes) {
+        // Code
+    }
+}
